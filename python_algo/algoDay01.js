7689e0a92718b192b84fb6997cced20c17bf1f25
Software Engineer <softwareengineer@LambdaSr233sMBP.home>
2020-09-28 10:39:54 -0400

    algoDay001 completed

---
+++ python_algo/algoDay01.js
@@ -0,0 +1,10 @@
+function reverseString(str){
+    var reversed = ""
+
+    for(var i = str.length -1; i>=0; i--){
+        reversed += str[i]
+    }
+    return reversed
+}
+
+console.log(reverseString("hello"))
